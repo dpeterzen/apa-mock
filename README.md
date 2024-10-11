86edf5d9a099f96da198022f8fd2cef48cc0ccae
Dan Peterson <dmpeterson317@gmail.com>
2024-10-11 00:17:24 -0400

    chores

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 - [x] add resize to all sides
 - [ ] cache responsive behavior
 - [ ] title nav component - container flex fill inner space - 
+- [] right click, ctrl zoom, green button, auto hide nav title
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -19,20 +19,12 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav className={`flex items-center justify-between p-4 ${styles.navbar} bg-background dark:border-neutral-900 ${isScrolled ? 'border-b' : ''}`}>
+    <nav className={`flex items-center justify-between px-3 h-14 ${styles.navbar} bg-background dark:border-neutral-900 ${isScrolled ? 'border-b' : ''}`}>
       {/* Left Section: Hamburger Button */}
       <Button className="border-0 hover:bg-background" variant="outline" size="icon">
+
         <svg
-          className="w-6 h-6"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M3 5H11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-          <path d="M3 12H16" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-          <path d="M3 19H21" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-        </svg>
+          className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-layout-dashboard"><rect width="7" height="9" x="3" y="3" rx="1"/><rect width="7" height="5" x="14" y="3" rx="1"/><rect width="7" height="9" x="14" y="12" rx="1"/><rect width="7" height="5" x="3" y="16" rx="1"/></svg>
       </Button>
       {/* Center Section: Carousel */}
       <div className="flex-1 flex justify-start">
